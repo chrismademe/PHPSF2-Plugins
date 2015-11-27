1ad37e2476ce942be46b77ace4c1018828ada97f
Chris <chris@chrismade.me>
2015-11-27 08:41:28 +0000

    Add filters

--- contact-form/plugin.php
+++ contact-form/plugin.php
@@ -56,50 +56,48 @@ if ( is_path( FORM_HANDLER ) && is_form_data() ) {
     /**
      * Filter Input
      */
-    $fields = $gump->filter($fields, [
+    $fields = $gump->filter($fields, array(
         'name'      => 'trim|sanitize_string',
         'email'     => 'trim|sanitize_email',
         'phone'     => 'trim|sanitize_numbers',
         'message'   => 'trim|sanitize_string',
         'validate'  => 'trim'
-    ]);
+    ));
 
     /**
      * Validate input
      */
-    $valid = $gump->validate($fields, [
+    $valid = $gump->validate($fields, array(
         'name'      => 'required|alpha_numeric',
         'email'     => 'required|valid_email',
         'phone'     => 'required|numeric',
         'message'   => 'required',
         'validate'  => 'required|numeric|exact_len,1'
-    ]);
+    ));
 
     if ( $valid === true ) {
 
-        /**
-         * Include Spam Filter
-         */
+        # Include Spam Filter
         require_once __DIR__.'/inc/spam-filter.php';
 
-        /**
-         * New ContactForm instance
-         */
+        # New ContactForm instance
         $contact = new ContactForm\ContactForm();
 
-        /**
-         * Check for spam then submit
-         */
+        # Check for spam then submit
         if ( !is_spam($fields['message']) ) {
             $contact->submit($fields);
         } else {
             $contact->setResponse('error', 'Your message has been marked as spam and not sent. If you believe this is a mistake, please call us.');
         }
 
-        /**
-         * Get response
-         */
-        $variables->add('contact_response', $contact->getResponse());
+        # Get Response
+        $response = $contact->getResponse();
+        
+        # Apply Filters
+        $response = apply_filters( 'contact_response', $response );
+        
+        # Pass to theme
+        $variables->add('contact_response', $response );
 
     } else {
         $variables->add('contact_response', $gump->get_readable_errors(true));
